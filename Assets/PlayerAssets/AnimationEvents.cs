844a0afb2b7c223f73ba5109ec0ee5cc75013ffa
Zanice <znj@case.edu>
2014-02-15 20:43:52 -0500

    Basic Combat and Integration into Animation

---
+++ Assets/PlayerAssets/AnimationEvents.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationEvents : MonoBehaviour {
+	public void Attack() {
+		//Aimed distance
+		RaycastHit hit;
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+	
+		if(Physics.Raycast(transform.position, fwd, out hit, 3)) {
+				print("Object encountered! (" + hit.distance + ")");
+				Debug.DrawLine(transform.position, hit.point, Color.red);
+				
+				//Destroy(hit.transform.gameObject);
+			}
+			else if (Physics.Raycast(transform.position, fwd, out hit, 100))
+					Debug.DrawLine(transform.position, hit.point, Color.green);
+	}
+}
--- Assets/PlayerAssets/Combat/CombatSystem.cs
+++ Assets/PlayerAssets/Combat/CombatSystem.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 
 public class CombatSystem : MonoBehaviour {
-	float distGround;
 	public static bool attacking;
 	
 	void Start() {
@@ -10,21 +9,8 @@ public class CombatSystem : MonoBehaviour {
 	}
 	
 	void Update() {
-		//Aimed distance
-		RaycastHit hit;
-		Vector3 fwd = transform.TransformDirection(Vector3.forward);
-		
-		if (Input.GetButton("Fire1")) {
-			if(Physics.Raycast(transform.position, fwd, out hit, 3)) {
-				print("Object encountered! (" + hit.distance + ")");
-				Debug.DrawLine(transform.position, hit.point, Color.red);
-				
-				//Destroy(hit.transform.gameObject);
-			}
-			else if (Physics.Raycast(transform.position, fwd, out hit, 100))
-					Debug.DrawLine(transform.position, hit.point, Color.green);
+		if (Input.GetButton("Fire1"))
 			attacking = true;
-		}
 		else
 			attacking = false;
 	}

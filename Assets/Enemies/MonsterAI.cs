5bbafe3f4e5b1f8f0bfa8120f4651b1f4e3049c4
dartonus <callum.grant@verizon.net>
2014-02-19 21:49:36 -0500

    Monster AI Improvements

--- Assets/Enemies/MonsterAI.cs
+++ Assets/Enemies/MonsterAI.cs
@@ -6,6 +6,10 @@ public class MonsterAI : MonoBehaviour {
 	CharacterController _controller;
 	Transform _transform;
 	Animator animator;
+	Transform _player;
+
+	Transform _eyes;
+	Transform _playerMarker;
 
 	float speed;
 	float gravity;
@@ -15,20 +19,31 @@ public class MonsterAI : MonoBehaviour {
 	float maxRotSpeed = 200.0f;
 	float minTime = 0.1f;
 	float velocity;
+
+	float wanderSpeed = 4f;
+	float chaseSpeed = 7f;
+	bool chasing;
 	
 	int health;
 
 	bool change;
 	float range;
+	float attackRange = 9f;
+	float squareRange = 160f;
 
 	// Use this for initialization
 	void Start () {
 		_controller = GetComponent<CharacterController>();
 		_transform = GetComponent<Transform> ();
 		animator = GetComponent<Animator>();
-		speed = 4f;
+		_player = GameObject.Find ("Player").GetComponent<Transform> ();
+		_eyes = _transform.Find ("Eyes");
+		_playerMarker = _player.Find ("PlayerMarker");
+		chasing = false;
+		speed = wanderSpeed;
 		gravity = 20f;
 		animator.SetFloat ("speed", speed);
+		animator.SetBool ("isgrounded", true);
 		range = 2f;
 		target = GetTarget ();
 		InvokeRepeating ("NewTarget", 0.01f, 1.0f);
@@ -38,23 +53,54 @@ public class MonsterAI : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (_transform.position.y < -5) {
-			DestroyImmediate(this.gameObject);
+			DestroyImmediate (this.gameObject);
 		}
-		if (change) {
-			target = GetTarget();
+		
+		if (chasing) {
+			if ((_player.position - _transform.position).sqrMagnitude < attackRange) {
+				animator.SetBool ("dojump", true);
+				//PROGRAM ATTACKING ROUTINE HERE
+			}
+			else{
+				animator.SetBool ("dojump", false);
+				if(Vector3.Distance (_transform.position, _player.position) > 3f){
+					moveDirection = _transform.forward;
+					moveDirection *= speed;
+					moveDirection.y -= gravity;
+					_controller.Move (moveDirection * Time.deltaTime);
+					var newRotation = Quaternion.LookRotation (_player.position - _transform.position).eulerAngles;
+					var angles = _transform.rotation.eulerAngles;
+					_transform.rotation = Quaternion.Euler (angles.x, Mathf.SmoothDampAngle (angles.y, newRotation.y, ref velocity, minTime, maxRotSpeed), angles.z);
+					animator.SetFloat ("speed", speed);
+				}
+				else{
+					animator.SetFloat ("speed", 0f);
+				}
+			}
 		}
-		if (Vector3.Distance (_transform.position, target) > range) {
-			moveDirection = _transform.forward;
-			moveDirection *= speed;
-			moveDirection.y -= gravity;
-			_controller.Move (moveDirection * Time.deltaTime);
-			var newRotation = Quaternion.LookRotation (target - _transform.position).eulerAngles;
-			var angles = _transform.rotation.eulerAngles;
-			_transform.rotation = Quaternion.Euler (angles.x, Mathf.SmoothDampAngle (angles.y, newRotation.y, ref velocity, minTime, maxRotSpeed), angles.z);
-			animator.SetFloat ("speed", speed);
-		}
-		else{
-			animator.SetFloat("speed", 0f);
+		else {
+			if((_player.position - _transform.position).sqrMagnitude < squareRange && (Vector3.Dot ((_player.position - _transform.position).normalized, _transform.forward) > 0)){
+				if(!Physics.Linecast (_eyes.position, _playerMarker.position)){
+					chasing = true;
+					speed = chaseSpeed;
+				}
+			}
+			if (change) {
+				target = GetTarget ();
+			}
+			if (Vector3.Distance (_transform.position, target) > range) {
+				moveDirection = _transform.forward;
+				moveDirection *= speed;
+				moveDirection.y -= gravity;
+				_controller.Move (moveDirection * Time.deltaTime);
+				var newRotation = Quaternion.LookRotation (target - _transform.position).eulerAngles;
+				var angles = _transform.rotation.eulerAngles;
+				_transform.rotation = Quaternion.Euler (angles.x, Mathf.SmoothDampAngle (angles.y, newRotation.y, ref velocity, minTime, maxRotSpeed), angles.z);
+				animator.SetFloat ("speed", speed);
+			} 
+			else {
+				animator.SetFloat ("speed", 0f);
+			}
 		}
 	}
 

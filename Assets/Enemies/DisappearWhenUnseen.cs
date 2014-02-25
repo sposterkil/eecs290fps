dc03c6ce400ecf941b8776c0b527ba3a80cd9028
Sam Osterkil <pisketch@gmail.com>
2014-02-25 13:53:39 -0500

    Script to destroy parent when it's unseen
    Useful for making corpses poof, in theory.

---
+++ Assets/Enemies/DisappearWhenUnseen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisappearWhenUnseen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(!gameObject.renderer.isVisible){
+			Object.Destroy(gameObject);
+		}
+	}
+}

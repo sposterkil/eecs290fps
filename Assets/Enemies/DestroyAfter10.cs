ef48c041daf905f14226cb1cb489396fe87c158d
dartonus <callum.grant@verizon.net>
2014-02-27 16:01:15 -0500

    Commented Ragdoll script

--- Assets/Enemies/DestroyAfter10.cs
+++ Assets/Enemies/DestroyAfter10.cs
@@ -2,11 +2,14 @@
 using System.Collections;
 
 public class DestroyAfter10 : MonoBehaviour {
+	//The Player's collider
 	Collider _player;
+	//The Ragdoll's collider
 	Collider _ragdoll;
 
 	/**
 	 * On initialization, sets the ragdoll to be destroyed in 10 seconds (presumably, the player will not be looking when this occurs)
+	 * Also sets the Ragdoll's collider to not collide with the Player. (Originally, the invisible collider left behind would act as a invisible pillar blocking the player)
 	 */
 	void Start () {
 		DestroyObject (this.gameObject, 10f);

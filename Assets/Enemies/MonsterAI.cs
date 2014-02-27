c5030327177fc14d94372d6775877ae56f420dd6
dartonus <callum.grant@verizon.net>
2014-02-27 15:10:45 -0500

    Some commenting on MonsterAI

--- Assets/Enemies/MonsterAI.cs
+++ Assets/Enemies/MonsterAI.cs
@@ -3,45 +3,76 @@ using System.Collections;
 
 [RequireComponent (typeof (CharacterController))]
 public class MonsterAI : MonoBehaviour {
+	//The Monster's CharacterController.
 	CharacterController _controller;
+	//The Monster's Transform.
 	Transform _transform;
+	//The Monster's animator
 	Animator animator;
+	//The Player's transform.
 	Transform _player;
+	//The PlayerManager of the Player, used to damage the player.
 	PlayerManager _playerManager;
+	//The ragdoll spawned when the Monster is killed.
 	public GameObject ragdoll;
 
+	//The sound a monster makes when aggroed.
 	public AudioSource growl;
+	//The sound a monster makes when attacking.
 	public AudioSource hit;
 
+	//The empty game object in front of the Monster's face, used to raycast from.
 	Transform _eyes;
+	//The empty game object just above the Player's head, used to raycast to.
 	Transform _playerMarker;
 
+	//The Monster's movement speed
 	float speed;
+	//The pull of gravity on the Monster.
 	float gravity;
+	//The direction the Monster is moving in.
 	Vector3 moveDirection;
 
+	//The Monster's current spot in the maze it is attempting to path to.
 	Vector3 target;
+	//The maximum speed a Monster can rotate at.
 	float maxRotSpeed = 200.0f;
+	//Used to determine rotation speed for smoothdampangle used when turning.
 	float minTime = 0.1f;
+	//Current velocity, used for smoothdampangle.
 	float velocity;
 
+	//The speed a Monster meanders through the maze at.
 	float wanderSpeed = 4f;
+	//The speed a Monster will pursue the player at.
 	float chaseSpeed = 8f;
+	//Whether or not the Monster is pursuing the player.
 	bool chasing;
 
+	//The Monster's health.
 	int health;
 
+	//Whether or not the Monster should try to path to a new location in the Maze.
 	bool change;
+	//The distance a Monster will approach its current pathing point before stopping.
 	float range;
+	//Range that a Monster can attack at.
 	float attackRange = 9f;
+	//Range of the Monster's eyesight, squared.
 	float squareRange = 160f;
 
+	//The frame delay before damage occurs when the monster is attacking. Used to sync damage with the animation.
 	int attackDelay = 33;
+	//The time until the next attack. Used to prevent the monsters from doing their damage every frame, rapidly draining the player's health.
 	int attackTimer;
+	//Damage monsters do to player Health.
 	int attackDamage = 4;
+	//Amount of battery drained by Monster attacks.
 	int batteryDamage = 2;
 
-	// Use this for initialization
+	/**
+	 * On initialization, sets the monster's various variables to their defaults and acquires transforms used in the Update method. 
+	 */
 	void Start () {
 		_controller = GetComponent<CharacterController>();
 		_transform = GetComponent<Transform> ();

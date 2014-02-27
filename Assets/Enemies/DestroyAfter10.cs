458901d00cc1ab6f413a43ac9136da8a5ab73bfa
dartonus <callum.grant@verizon.net>
2014-02-27 14:54:57 -0500

    Commented Ragdoll Destruction script

--- Assets/Enemies/DestroyAfter10.cs
+++ Assets/Enemies/DestroyAfter10.cs
@@ -3,12 +3,16 @@ using System.Collections;
 
 public class DestroyAfter10 : MonoBehaviour {
 
-	// Use this for initialization
+	/**
+	 * On initialization, sets the ragdoll to be destroyed in 10 seconds (presumably, the player will not be looking when this occurs)
+	 */
 	void Start () {
 		DestroyObject (this.gameObject, 10f);
 	}
 
-	// Update is called once per frame
+	/**
+	 * Ragdolls just lie around and do nothing, so the Update method is nonfunctional.
+	 */
 	void Update () {
 	}
 }
